92ff82c58f6e4ef05376a3539b3ed4244147b319
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2015-11-28 13:35:32 +0300

    Add model classes for 'ping' and 'push' webhook payloads

---
+++ src/main/java/org/eclipse/egit/github/core/event/PingWebHookPayload.java
@@ -0,0 +1,60 @@
+package org.eclipse.egit.github.core.event;
+
+import org.eclipse.egit.github.core.RepositoryHook;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.User;
+
+import java.io.Serializable;
+
+/**
+ * 'ping' webhook event payload model class.
+ */
+public class PingWebHookPayload implements Serializable {
+    private static final long serialVersionUID = -1542484898545583479L;
+
+    private String zen;
+    private Integer hook_id;
+    private RepositoryHook hook;
+    private Repository repository;
+    private User sender;
+
+    public String getZen() {
+        return zen;
+    }
+
+    public void setZen(String zen) {
+        this.zen = zen;
+    }
+
+    public Integer getHook_id() {
+        return hook_id;
+    }
+
+    public void setHook_id(Integer hook_id) {
+        this.hook_id = hook_id;
+    }
+
+    public RepositoryHook getHook() {
+        return hook;
+    }
+
+    public void setHook(RepositoryHook hook) {
+        this.hook = hook;
+    }
+
+    public Repository getRepository() {
+        return repository;
+    }
+
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+}
---
+++ src/main/java/org/eclipse/egit/github/core/event/PushWebHookPayload.java
@@ -0,0 +1,143 @@
+package org.eclipse.egit.github.core.event;
+
+import org.eclipse.egit.github.core.Commit;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.User;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 'push' webhook event payload model class.
+ */
+public class PushWebHookPayload implements Serializable {
+
+    private static final long serialVersionUID = -1542484898545583478L;
+
+    private String ref;
+
+    private String before;
+    private String after;
+
+    // Either head or head_commit
+    private String head;
+    private Commit head_commit;
+
+    private boolean created;
+    private boolean deleted;
+    private boolean forced;
+
+    private List<Commit> commits;
+    private Repository repository;
+
+    private User pusher;
+    private User sender;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public String getBefore() {
+        return before;
+    }
+
+    public String getAfter() {
+        return after;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public Commit getHead_commit() {
+        return head_commit;
+    }
+
+    public boolean isCreated() {
+        return created;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public boolean isForced() {
+        return forced;
+    }
+
+    public List<Commit> getCommits() {
+        return commits;
+    }
+
+    public Repository getRepository() {
+        return repository;
+    }
+
+    public User getPusher() {
+        return pusher;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public PushWebHookPayload setRef(String ref) {
+        this.ref = ref;
+        return this;
+    }
+
+    public PushWebHookPayload setBefore(String before) {
+        this.before = before;
+        return this;
+    }
+
+    public PushWebHookPayload setAfter(String after) {
+        this.after = after;
+        return this;
+    }
+
+    public PushWebHookPayload setHead(String head) {
+        this.head = head;
+        return this;
+    }
+
+    public PushWebHookPayload setHead_commit(Commit head_commit) {
+        this.head_commit = head_commit;
+        return this;
+    }
+
+    public PushWebHookPayload setCreated(boolean created) {
+        this.created = created;
+        return this;
+    }
+
+    public PushWebHookPayload setDeleted(boolean deleted) {
+        this.deleted = deleted;
+        return this;
+    }
+
+    public PushWebHookPayload setForced(boolean forced) {
+        this.forced = forced;
+        return this;
+    }
+
+    public PushWebHookPayload setCommits(List<Commit> commits) {
+        this.commits = commits;
+        return this;
+    }
+
+    public PushWebHookPayload setRepository(Repository repository) {
+        this.repository = repository;
+        return this;
+    }
+
+    public PushWebHookPayload setPusher(User pusher) {
+        this.pusher = pusher;
+        return this;
+    }
+
+    public PushWebHookPayload setSender(User sender) {
+        this.sender = sender;
+        return this;
+    }
+}
