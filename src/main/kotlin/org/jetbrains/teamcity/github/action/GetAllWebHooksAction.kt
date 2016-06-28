09866b9632f5f71b7cd3f4fb53f57c2912c22c2d
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2016-06-29 00:05:05 +0300

    Add logging for GetAllWebHooksAction

--- src/main/kotlin/org/jetbrains/teamcity/github/action/GetAllWebHooksAction.kt
+++ src/main/kotlin/org/jetbrains/teamcity/github/action/GetAllWebHooksAction.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.teamcity.github.action
 
+import com.intellij.openapi.diagnostic.Logger
 import jetbrains.buildServer.serverSide.oauth.github.GitHubClientEx
 import org.eclipse.egit.github.core.client.RequestException
 import org.eclipse.egit.github.core.service.RepositoryService
@@ -7,16 +8,21 @@ import org.jetbrains.teamcity.github.GitHubAccessException
 import org.jetbrains.teamcity.github.GitHubRepositoryInfo
 import org.jetbrains.teamcity.github.TokensHelper
 import org.jetbrains.teamcity.github.callbackUrl
+import java.net.HttpURLConnection.*
 
 /**
  * Fetches all webhooks points to this server for given repository
  */
 object GetAllWebHooksAction {
+
+    private val LOG = Logger.getInstance(GetAllWebHooksAction::class.java.name)
+
     @Throws(GitHubAccessException::class)
     fun doRun(info: GitHubRepositoryInfo, client: GitHubClientEx, context: ActionContext): HooksGetOperationResult {
         val service = RepositoryService(client)
         val repo = info.getRepositoryId()
         try {
+            LOG.debug("Loading webhooks for repository $info")
             val hooks = service.getHooks(repo)
             val filtered = hooks.filter {
                 val url = it.callbackUrl
@@ -26,13 +32,23 @@ object GetAllWebHooksAction {
                 && url.startsWith(context.getCallbackUrl())
                 && "json" == it.config["content_type"]
             }
+            val active = filtered.filter { it.isActive }
+            if (filtered.size > 0) {
+                LOG.debug("Found ${filtered.size} webhook for repository $info; ${active.size} - active; ${hooks.size - filtered.size} - other hooks")
+            } else {
+                LOG.debug("No webhooks found for repository $info")
+            }
+            if (active.size > 1) {
+                LOG.info("More than one (${active.size} active webhooks found for repository $info")
+            }
             context.updateHooks(info.server, repo, filtered)
         } catch(e: RequestException) {
+            LOG.warnAndDebugDetails("Failed to load webhooks for repository $info: ${e.status}", e)
             when (e.status) {
-                401 -> {
+                HTTP_UNAUTHORIZED -> {
                     throw GitHubAccessException(GitHubAccessException.Type.InvalidCredentials)
                 }
-                403, 404 -> {
+                HTTP_NOT_FOUND, HTTP_FORBIDDEN -> {
                     // No access
                     // Probably token does not have permissions
                     val scopes = client.tokenOAuthScopes?.map { it.toLowerCase() } ?: throw GitHubAccessException(GitHubAccessException.Type.NoAccess) // Weird. No header?
@@ -43,6 +59,10 @@ object GetAllWebHooksAction {
                         TokensHelper.HookAccessType.READ -> throw GitHubAccessException(GitHubAccessException.Type.TokenScopeMismatch)
                         TokensHelper.HookAccessType.WRITE, TokensHelper.HookAccessType.ADMIN -> throw GitHubAccessException(GitHubAccessException.Type.UserHaveNoAccess)
                     }
+                }
+                HTTP_INTERNAL_ERROR -> {
+                    // GH error, try later
+                    // TODO: Throw proper exception
                 }
             }
             throw e
