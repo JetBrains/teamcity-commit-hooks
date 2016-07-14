0788fb01f17a803c964128a22f345c45f95efe02
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2016-07-14 17:15:23 +0300

    Do not force popup in 'Incorrect WebHook' health item

--- src/main/kotlin/org/jetbrains/teamcity/github/GitHubWebHookIncorrectPageExtension.kt
+++ src/main/kotlin/org/jetbrains/teamcity/github/GitHubWebHookIncorrectPageExtension.kt
@@ -8,10 +8,12 @@ import jetbrains.buildServer.web.openapi.PagePlaces
 import jetbrains.buildServer.web.openapi.PluginDescriptor
 import jetbrains.buildServer.web.openapi.healthStatus.HealthStatusItemPageExtension
 import jetbrains.buildServer.web.util.SessionUser
+import org.springframework.beans.factory.annotation.Autowired
 import javax.servlet.http.HttpServletRequest
 
 class GitHubWebHookIncorrectPageExtension(descriptor: PluginDescriptor, places: PagePlaces) : HealthStatusItemPageExtension(WebhookPeriodicalChecker.TYPE, places) {
-
+    @Autowired
+    lateinit var helper: TokensHelper
     private val myResourcesPath = descriptor.pluginResourcesPath
 
     init {
@@ -24,14 +26,9 @@ class GitHubWebHookIncorrectPageExtension(descriptor: PluginDescriptor, places: 
         val item = getStatusItem(request)
         val user = SessionUser.getUser(request) ?: return false
         @Suppress("UNCHECKED_CAST")
-        val projects: Set<SProject>? = item.additionalData["Projects"] as Set<SProject>?
-        val project = projects?.firstOrNull { user.isPermissionGrantedForProject(it.projectId, Permission.EDIT_PROJECT) }
-        @Suppress("IfNullToElvis")
-        if (project == null) {
-            // User cannot edit at least one project with GitHub repo / VCS root declaration
-            return false
-        }
-        return true
+        val projects = item.additionalData["Projects"] as Set<SProject>? ?: return false
+        // User cannot edit at least one project with GitHub repo / VCS root declaration
+        return projects.any { user.isPermissionGrantedForProject(it.projectId, Permission.EDIT_PROJECT) }
     }
 
     override fun fillModel(model: MutableMap<String, Any>, request: HttpServletRequest) {
@@ -41,14 +38,27 @@ class GitHubWebHookIncorrectPageExtension(descriptor: PluginDescriptor, places: 
 
         @Suppress("UNCHECKED_CAST")
         val projects: Set<SProject> = item.additionalData["Projects"] as Set<SProject>
+        val info: GitHubRepositoryInfo = item.additionalData["GitHubInfo"] as GitHubRepositoryInfo
 
         val projectsSorted = projects.toMutableList()
         if (projectsSorted.size > 1) {
             projectsSorted.sortWith(ProjectComparator(false))
         }
+        val editableSortedProjects = projectsSorted.filter {
+            user.isPermissionGrantedForProject(it.projectId, Permission.EDIT_PROJECT)
+        }
 
-        val project = projectsSorted.firstOrNull { user.isPermissionGrantedForProject(it.projectId, Permission.EDIT_PROJECT) }!!
+        val project = editableSortedProjects.firstOrNull {
+            val connections = helper.getConnections(it, info.server)
+            val tokens = helper.getExistingTokens(connections, user)
+            tokens.isNotEmpty()
+        } ?: editableSortedProjects.first()
         model["Project"] = project
+
+        val connections = helper.getConnections(project, info.server)
+        model.put("has_connections", connections.isNotEmpty())
+        val tokens = helper.getExistingTokens(connections, user)
+        model.put("has_tokens", tokens.isNotEmpty())
 
         model["gson"] = Gson()
         model["pluginResourcesPath"] = myResourcesPath
