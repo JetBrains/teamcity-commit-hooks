857c330b5ef2733d856b38baef9dd375efa0ebaa
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2021-03-29 17:22:23 +0100

    Minor cleanup in webhook storage test

--- src/test-integration/kotlin/org/jetbrains/teamcity/github/WebHooksStorageIntegrationTest.kt
+++ src/test-integration/kotlin/org/jetbrains/teamcity/github/WebHooksStorageIntegrationTest.kt
@@ -10,7 +10,7 @@ import org.jetbrains.teamcity.github.controllers.Status
 
 class WebHooksStorageIntegrationTest: BaseServerTestCase() {
 
-    private var hookStorage: WebHooksStorage? = null
+    private lateinit var hookStorage: WebHooksStorage
 
     @BeforeMethod
     override fun setUp() {
@@ -20,61 +20,61 @@ class WebHooksStorageIntegrationTest: BaseServerTestCase() {
     }
 
     @Test
-    fun `test add hook`() {
-        val hook = repoHook(123,  "http://fake.callback.url")
-        val info = hookStorage?.getOrAdd(hook)
+    fun `add hook`() {
+        val hook = repoHook(123)
+        val info = hookStorage.getOrAdd(hook)
         then(info).isNotNull()
-        then(info?.status).isEqualTo(Status.WAITING_FOR_SERVER_RESPONSE)
-        val all = hookStorage?.getAll()
-        then(all?.map { it.second }).isEqualTo(listOf(info))
+        then(info.status).isEqualTo(Status.WAITING_FOR_SERVER_RESPONSE)
+        val all = hookStorage.getAll()
+        then(all.map { it.second }).isEqualTo(listOf(info))
     }
 
     @Test
-    fun `test delete hook`() {
-        val info123 = hookStorage?.getOrAdd(repoHook(123,  "http://fake.callback.url"))
-        val info345 = hookStorage?.getOrAdd(repoHook(345,  "http://fake.callback.url"))
-        then(hookStorage?.getAll()?.map { it.second }).hasSize(2)
-        hookStorage?.delete(info123!!)
-        then(hookStorage?.getAll()?.map { it.second }).isEqualTo(listOf(info345))
+    fun `delete hook`() {
+        val info123 = hookStorage.getOrAdd(repoHook(123))
+        val info345 = hookStorage.getOrAdd(repoHook(345))
+        then(hookStorage.getAll().map { it.second }).hasSize(2)
+        hookStorage.delete(info123)
+        then(hookStorage.getAll().map { it.second }).isEqualTo(listOf(info345))
     }
 
     @Test
-    fun `test 2 hooks`() {
-        val hook = repoHook(123,  "http://fake.callback.url")
-        hookStorage?.getOrAdd(hook)
-        then(hookStorage?.getAll()).hasSize(1)
-        val hook2 = repoHook(345,  "http://fake.callback.url")
-        hookStorage?.getOrAdd(hook2)
-        then(hookStorage?.getAll()).hasSize(2)
+    fun `add 2 hooks`() {
+        val hook = repoHook(123)
+        hookStorage.getOrAdd(hook)
+        then(hookStorage.getAll()).hasSize(1)
+        val hook2 = repoHook(345)
+        hookStorage.getOrAdd(hook2)
+        then(hookStorage.getAll()).hasSize(2)
     }
 
     @Test
-    fun `test 2 identical hooks`() {
-        val hook = repoHook(123,  "http://fake.callback.url")
-        hookStorage?.getOrAdd(hook)
-        then(hookStorage?.getAll()).hasSize(1)
-        val hook2 = repoHook(123,  "http://fake.callback.url")
-        hookStorage?.getOrAdd(hook2)
-        then(hookStorage?.getAll()).hasSize(1)
+    fun `add 2 identical hooks`() {
+        val hook = repoHook(123)
+        hookStorage.getOrAdd(hook)
+        then(hookStorage.getAll()).hasSize(1)
+        val hook2 = repoHook(123)
+        hookStorage.getOrAdd(hook2)
+        then(hookStorage.getAll()).hasSize(1)
     }
 
     @Test
-    fun `test hook saved to disk`() {
-        val hook = repoHook(123,  "http://fake.callback.url")
-        val info = hookStorage?.getOrAdd(hook)
+    fun `add hook saves to disk`() {
+        val hook = repoHook(123)
+        val info = hookStorage.getOrAdd(hook)
         then(info).isNotNull()
-        then(info?.status).isEqualTo(Status.WAITING_FOR_SERVER_RESPONSE)
-        val f = hookStorage?.getStorageFile()
-        waitFor({ f?.exists()!! }, 10000L)
-        then(f?.readText()).contains("hooks/123")
+        then(info.status).isEqualTo(Status.WAITING_FOR_SERVER_RESPONSE)
+        val f = hookStorage.getStorageFile()
+        waitFor({ f.exists() }, 10000L)
+        then(f.readText()).contains("hooks/123")
     }
 
-    private fun repoHook(hookId: Long, callbackUrl: String): RepositoryHook {
+    private fun repoHook(hookId: Long): RepositoryHook {
         val hook = RepositoryHook()
         hook.id = hookId
         hook.url = "https://api.github.com/repos/myowner/myrepo/hooks/${hookId}"
         hook.config = HashMap<String, String>()
-        hook.config["url"] = callbackUrl
+        hook.config["url"] = "http://fake.callback.url"
         hook.isActive = true
         return hook
     }
